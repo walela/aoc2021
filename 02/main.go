046f8ae4f51bfa0840d1b60c272b7f952efccd98
Austin Walela <austinmasibo@gmail.com>
2021-12-02 18:31:07 +0300

    first pass

--- 02/main.go
+++ 02/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,4 +15,25 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
+
+	depth := 0
+	alignment := 0
+	s := bufio.NewScanner(file)
+
+	for s.Scan() {
+		command := strings.Split(s.Text(), " ")
+		switch command[0] {
+		case "forward":
+			delta, _ := strconv.Atoi(command[1])
+			alignment += delta
+		case "up":
+			delta, _ := strconv.Atoi(command[1])
+			depth -= delta
+		case "down":
+			delta, _ := strconv.Atoi(command[1])
+			depth += delta
+		default:
+		}
+	}
+	fmt.Println(depth * alignment)
 }
